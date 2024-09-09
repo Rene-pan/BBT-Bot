d95af62a800567858743060f3f42b1b7bcbfea65
Rene Pan <renepanyuxuan@gmail.com>
2024-09-09 17:39:22 +0800

    Added 3rd Person Camera

---
+++ GAM300/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform followTarget;
+    [SerializeField] float cameraZDistance;//bigger = viewport will be bigger, smaller = viewport will be smaller
+    [SerializeField] float minVerticalAngle = -45;
+    [SerializeField] float maxVerticalAngle = 45;
+    float rotationX;
+    float rotationY;
+
+    private void Update()
+    {
+        if (followTarget != null)
+        {
+            rotationX += Input.GetAxis("Mouse Y");
+            rotationX = Mathf.Clamp(rotationX, minVerticalAngle, maxVerticalAngle);
+            rotationY += Input.GetAxis("Mouse X");
+            var targetRotation = Quaternion.Euler(rotationX, rotationY, 0);
+            //make sure camera is always behind player and how far it is looking at, Rotation By cameraAngle 
+            transform.position = followTarget.position - Quaternion.Euler(rotationX, rotationY, 0) * new Vector3(0, 0, cameraZDistance);
+            transform.rotation = targetRotation;
+        }
+    }
+}
