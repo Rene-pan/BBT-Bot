ed4134309599bd9af0f2a449656a167a78b9d226
Rene Pan <renepanyuxuan@gmail.com>
2024-09-21 03:06:24 +0800

    Added Customer AI & make it move to the seat and sits down

--- GAM300/Assets/Scripts/Customer.cs
+++ GAM300/Assets/Scripts/Customer.cs
@@ -1,8 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using static Customer;
 
 public class Customer : MonoBehaviour
 {
-  
+    public enum Customerstates { ORDER, WAIT, EAT, ANGRY, LEAVE }
+    [SerializeField] Customerstates currentstate = Customerstates.ORDER;
+    NavMeshAgent agent;
+    public Transform[] waypoints;
+    int waypointIndex;
+    Vector3 target;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        UpdateDestination();
+    }
+    private void Update()
+    {
+        switch (currentstate)
+        {
+            case Customerstates.ORDER:
+                if (Vector3.Distance(transform.position, target) < 1.5f)
+                {
+                    UpdateDestination();
+                }
+                break;
+            case Customerstates.WAIT:
+                agent.speed = 0;
+                break;
+            case Customerstates.EAT:
+                break; 
+            case Customerstates.ANGRY:
+                break;
+            case Customerstates.LEAVE:
+                break;
+        }
+    }
+    public void ChangeState(Customerstates newState)
+    {
+        if (currentstate != newState)
+        {
+            currentstate = newState;
+        }
+    }
+    void UpdateDestination()
+    {
+        target = waypoints[waypointIndex].position;
+        agent.SetDestination(target);
+    }
+    public void IterateWayPointIndex()
+    {
+        waypointIndex++;
+        if (waypointIndex ==  waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
+
+    //public static void ForceCleanupNavMesh()
+    //{
+    //    if (Application.isPlaying)
+    //        return;
+
+    //    NavMesh.RemoveAllNavMeshData();
+    //}
 }
--- GAM300/Assets/Scripts/CustomerChair.cs
+++ GAM300/Assets/Scripts/CustomerChair.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CustomerChair : MonoBehaviour
 {
-    
+    private void OnTriggerStay(Collider other)
+    {
+        var tag = other.tag;
+        switch (tag) 
+        {
+            case "Customer":
+                print("customer reached");
+                var seatPivot = gameObject.transform.GetChild(0).transform;
+                Parent(seatPivot, other.gameObject, 0);
+                other.gameObject.transform.localPosition = Vector3.zero;
+                other.GetComponent<Customer>().ChangeState(Customer.Customerstates.WAIT);
+                break;
+        
+        }
+
+    }
+    private void Parent(Transform Parent, GameObject child, int state)
+    {
+        switch (state)
+        {
+            case 0:
+                child.transform.SetParent(Parent);
+                break;
+            case 1:
+                child.transform.SetParent(null);
+                break;
+        }
+    }
 }
