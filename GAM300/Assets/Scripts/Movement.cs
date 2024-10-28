046b9fecac1b46429b148bfcb76028264b47c064
Rene Pan <renepanyuxuan@gmail.com>
2024-10-28 16:32:43 +0800

    Added new Movement method

--- GAM300/Assets/Scripts/Movement.cs
+++ GAM300/Assets/Scripts/Movement.cs
@@ -30,23 +30,54 @@ public class Movement : MonoBehaviour
     }
     void Move()
     {
-            PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized;
-            MoveVector = transform.TransformDirection(PlayerMovementInput) * moveSpeed;
-            playerbody.velocity = new Vector3(MoveVector.x, playerbody.velocity.y, MoveVector.z);
+        //    PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized;
+        //    MoveVector = transform.TransformDirection(PlayerMovementInput) * moveSpeed;
+        //    playerbody.velocity = new Vector3(MoveVector.x, playerbody.velocity.y, MoveVector.z);
 
-        if (PlayerMovementInput.magnitude >= 0.1f && cam.gameObject.GetComponent<CamController_v3>().currentState == CamController_v3.CamState.THIRDPERSON)
+        //if (PlayerMovementInput.magnitude >= 0.1f && cam.gameObject.GetComponent<CamController_v3>().currentState == CamController_v3.CamState.THIRDPERSON)
+        //{
+        //    float targetAngle = Mathf.Atan2(PlayerMovementInput.x, 0) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        //    float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        //    transform.rotation = Quaternion.Euler(0, angle, 0);
+        //    Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+        //}
+        //if (PlayerMovementInput.magnitude >= 0.1f && cam.gameObject.GetComponent<CamController_v3>().currentState == CamController_v3.CamState.OVERSHOULDER)
+        //{
+        //    float targetAngle = Mathf.Atan2(PlayerMovementInput.x,0) * Mathf.Rad2Deg + Newcam.eulerAngles.y;
+        //    float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        //    transform.rotation = Quaternion.Euler(0, angle, 0);
+        //    Vector3 moveDir = Quaternion.Euler(0, 0, 0) * Vector3.back;
+        //}
+        // Get player input
+        PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized;
+
+        // Check if there is any movement input
+        if (PlayerMovementInput.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(PlayerMovementInput.x, 0) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0, angle, 0);
-            Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            float targetAngle = 0.0f;
+            // Calculate movement direction relative to the camera
+            if (cam.gameObject.GetComponent<CamController_v3>().currentState == CamController_v3.CamState.THIRDPERSON)
+                targetAngle = Mathf.Atan2(PlayerMovementInput.x, PlayerMovementInput.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            else if (cam.gameObject.GetComponent<CamController_v3>().currentState == CamController_v3.CamState.OVERSHOULDER)
+                targetAngle = Mathf.Atan2(PlayerMovementInput.x, PlayerMovementInput.z) * Mathf.Rad2Deg + Newcam.eulerAngles.y;
+
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            // Rotate the player gradually to face the movement direction
+            // Over the shoulder has that weird camera shake when moving, so this will only apply to 3rd person camera
+            if (cam.gameObject.GetComponent<CamController_v3>().currentState == CamController_v3.CamState.THIRDPERSON)
+                transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
+
+            // Move the player in the direction the camera is facing
+            Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+
+            MoveVector = moveDirection * moveSpeed;
+            playerbody.velocity = new Vector3(MoveVector.x, playerbody.velocity.y, MoveVector.z);
         }
-        if (PlayerMovementInput.magnitude >= 0.1f && cam.gameObject.GetComponent<CamController_v3>().currentState == CamController_v3.CamState.OVERSHOULDER)
+        else
         {
-            float targetAngle = Mathf.Atan2(PlayerMovementInput.x,0) * Mathf.Rad2Deg + Newcam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0, angle, 0);
-            Vector3 moveDir = Quaternion.Euler(0, 0, 0) * Vector3.back;
+            // If no input, maintain player's vertical velocity and stop horizontal movement
+            playerbody.velocity = new Vector3(0, playerbody.velocity.y, 0);
         }
     }
 
